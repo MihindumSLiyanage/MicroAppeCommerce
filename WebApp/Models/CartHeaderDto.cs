37f2380687837bfb070cb51395ca54e1e2bce80e
Mihindum <mihindum564@example.com>
2024-01-30 17:16:32 +0530

    Complete Frontend for OrderAPI

--- WebApp/Models/CartHeaderDto.cs
+++ WebApp/Models/CartHeaderDto.cs
@@ -1,4 +1,6 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class CartHeaderDto
     {
@@ -7,8 +9,11 @@
         public string? CouponCode { get; set; }
         public double Discount { get; set; }
         public double CartTotal { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? Phone { get; set; }
+        [Required]
         public string? Email { get; set; }
     }
 }
